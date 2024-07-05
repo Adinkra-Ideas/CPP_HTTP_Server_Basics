613b170478d5a16b047af36f92ba88a6f2e6f03f
euyi <euyi@student.42wolfsburg.de>
2024-07-05 13:45:00 +0530

    Refactored main.cpp
     Date:      Fri Jul 5 13:45:00 2024 +0530
    	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@
 /*   By: euyi <euyi@student.42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 19:03:12 by euyi              #+#    #+#             */
-/*   Updated: 2023/02/18 19:03:31 by euyi             ###   ########.fr       */
+/*   Updated: 2024/07/13 21:49:00 by euyi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "TcpServer.hpp"
 
 int main() {
-	http::TcpServer server("127.0.0.1", 8080);
-	server.startListen();
+	http::TcpServer server("127.0.0.1", 8080) ;
+	server.startListen() ;
 
 	return 0;
 }
